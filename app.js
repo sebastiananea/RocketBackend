4da4194676ca59ac8f9a7417d89319dbaf54dba7
SebastianAnea <sebastiananea@gmail.com>
2021-12-05 19:56:11 -0300

    origin cors cambiado en app.js

--- app.js
+++ app.js
@@ -1,43 +1,46 @@
-require("dotenv").config();
-const morgan = require("morgan");
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const cookieparser = require("cookie-parser");
-const axios = require("axios");
-const Profile = require("./models/Profiles.js");
-const Institution = require("./models/Institution.js");
-const routes = require("./routes/index");
-const { appConfig } = require("./Config/default");
+require('dotenv').config()
+const morgan = require('morgan')
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const cookieparser = require('cookie-parser')
+const axios = require('axios')
+const Profile = require('./models/Profiles.js')
+const Institution = require('./models/Institution.js')
+const routes = require('./routes/index')
+const { appConfig } = require('./Config/default')
 
 //RocketAppFranco
 //${DB_PASSWORD}
 
-const app = express();
-app.use(express.json());
-app.use(morgan("dev"));
-app.use(cors());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-mongoose.connect(
+const app = express()
+app.use(express.json())
+app.use(morgan('dev'))
+app.use(cors({ origin: 'https://rocketprojectarg.netlify.app/' }))
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  next()
+})
+mongoose
+  .connect(
     `mongodb+srv://apiAdmin:${appConfig.dbPass}@rocketapp.rnqqh.mongodb.net/Rocket?retryWrites=true&w=majority`,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     }
   )
-  .then(() => console.log("Connected to MongoDB"))
-  .catch(console.error);
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(console.error)
 
+app.use(express.urlencoded({ extended: true }))
 
-app.use(express.urlencoded({ extended: true }));
-
-app.use(cookieparser());
-app.use("/", routes);
+app.use(cookieparser())
+app.use('/', routes)
 
 app.listen(process.env.PORT || appConfig.port, () => {
-  console.log(`Conected to ${appConfig.port}`);
-});
+  console.log(`Conected to ${appConfig.port}`)
+})
