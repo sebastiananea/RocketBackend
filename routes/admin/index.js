254d79ce0e2209b0bb20edf2bcc7c3a77ee2046b
SebastianAnea <sebastiananea@gmail.com>
2021-12-09 19:32:55 -0300

    rutas report y like ready como el guille

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,10 +1,30 @@
 const { Router } = require('express')
 const router = Router()
 const Institution = require('../../models/Institution')
+const Likes = require('../../models/Likes')
+const Reports = require('../../models/Reports')
 
 router.get('/getCohortes', async (req, res) => {
   let cohortes = await Institution.find({ name: 'Henry' })
   res.status(200).send(cohortes[0].groups)
+})
+
+router.post('/like', async (req, res) => {
+  const { group, date } = req.body
+  var newLike = await new Likes({
+    group,
+    date,
+  })
+  newLike.save()
+})
+
+router.post('/report', async (req, res) => {
+  const { group, date } = req.body
+  var newReport = await new Reports({
+    group,
+    date,
+  })
+  newReport.save()
 })
 
 module.exports = router
