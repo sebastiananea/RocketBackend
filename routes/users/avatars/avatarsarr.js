fb678126af97bde18264a1ac19bf0bc8a7af70d2
Guillermo L. Martos <guille.l.martos@gmail.com>
2021-12-10 11:43:33 -0300

    loaded utils avatars con link nube

--- routes/users/avatars/avatarsarr.js
+++ routes/users/avatars/avatarsarr.js
@@ -1,31 +1,52 @@
-var Module = require('module');
-var fs     = require('fs');
+// var Module = require('module');
+// var fs     = require('fs');
 
-Module._extensions['.png'] = function(module, fn) {
-  var base64 = fs.readFileSync(fn).toString('base64');
-  module._compile('module.exports="data:image/jpg;base64,' + base64 + '"', fn);
-};
+// Module._extensions['.png'] = function(module, fn) {
+//   var base64 = fs.readFileSync(fn).toString('base64');
+//   module._compile('module.exports="data:image/jpg;base64,' + base64 + '"', fn);
+// };
 
-const avatar1 = require("./avatar1.png");
-const avatar2 = require("./avatar2.png");
-const avatar3 = require("./avatar4-mujer.png");
-const avatar4 = require("./avatar5.png");
-const avatar5 = require("./avatar6-mujer.png");
-const avatar6 = require("./avatar7.png");
-const avatar7 = require("./avatar8.png");
-const avatar8 = require("./avatar9-mujer.png");
-const avatar9 = require("./avatar10-mujer.png");
-const avatar10 = require("./avatar12-mujer.png");
-const avatar11 = require("./avatar13-mujer.png");
-const avatar12 = require("./avatar14-mujer.png");
-const avatar13 = require("./avatar15-mujer.png");
-const avatar14 = require("./avatar16.png");
-const avatar15 = require("./avatar17.png");
-const avatar16 = require("./avatar18.png");
-const avatar17 = require("./avatar19-cat.png");
-const avatar18 = require("./avatar19-mujer.png");
-const avatar19 = require("./avatar20-dog.png");
-const avatar20 = require("./avatar21-alien.png");
+// const avatar1 = require("./avatar1.png");
+// const avatar2 = require("./avatar2.png");
+// const avatar3 = require("./avatar4-mujer.png");
+// const avatar4 = require("./avatar5.png");
+// const avatar5 = require("./avatar6-mujer.png");
+// const avatar6 = require("./avatar7.png");
+// const avatar7 = require("./avatar8.png");
+// const avatar8 = require("./avatar9-mujer.png");
+// const avatar9 = require("./avatar10-mujer.png");
+// const avatar10 = require("./avatar12-mujer.png");
+// const avatar11 = require("./avatar13-mujer.png");
+// const avatar12 = require("./avatar14-mujer.png");
+// const avatar13 = require("./avatar15-mujer.png");
+// const avatar14 = require("./avatar16.png");
+// const avatar15 = require("./avatar17.png");
+// const avatar16 = require("./avatar18.png");
+// const avatar17 = require("./avatar19-cat.png");
+// const avatar18 = require("./avatar19-mujer.png");
+// const avatar19 = require("./avatar20-dog.png");
+// const avatar20 = require("./avatar21-alien.png");
+
+const avatar1 = "https://i.ibb.co/zhdg7nn/Avatar1.png" 
+const avatar2 = "https://i.ibb.co/SsxG64B/Avatar2.png"
+const avatar3 = "https://i.ibb.co/Z1YZ4sZ/Avatar4-mujer.png"
+const avatar4 = "https://i.ibb.co/fFyXKHd/Avatar5.png" 
+const avatar5 = "https://i.ibb.co/yYBXYfh/Avatar6-mujer.png"
+const avatar6 = "https://i.ibb.co/SvVY52w/Avatar7.png" 
+const avatar7 = "https://i.ibb.co/0cQtF3v/Avatar8.png" 
+const avatar8 = "https://i.ibb.co/wY2mgkG/Avatar9-mujer.png"
+const avatar9 = "https://i.ibb.co/Gsx6r3j/Avatar10-mujer.png"
+const avatar10 ="https://i.ibb.co/yNx0XxG/Avatar12-mujer.png" 
+const avatar11 = "https://i.ibb.co/61zsw1n/Avatar13-mujer.png"
+const avatar12 = "https://i.ibb.co/xYTHghy/Avatar14-mujer.png"
+const avatar13 = "https://i.ibb.co/ckb6Ynh/Avatar15-mujer.png"
+const avatar14 = "https://i.ibb.co/ckjFz35/Avatar16.png" 
+const avatar15 = "https://i.ibb.co/S6mSG41/Avatar17.png" 
+const avatar16 = "https://i.ibb.co/RzNTHnR/Avatar18.png" 
+const avatar17 = "https://i.ibb.co/ZMT1FpP/Avatar19-cat.png"
+const avatar18 = "https://i.ibb.co/GW4xYjD/Avatar19-mujer.png"
+const avatar19 = "https://i.ibb.co/0f2G5gH/Avatar20-dog.jpg"
+const avatar20 = "https://i.ibb.co/k9qYZqP/Avatar21-alien.jpg"
 
 const arr = [
   avatar1,
